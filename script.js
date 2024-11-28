4e664ecaf58b53f4db5fbe4a6f10f54a7a3f7cad
Deepsky709 <ferdinand.amadeus123@gmail.com>
2024-11-28 17:32:26 +0700

    1.3

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-let username = document.getElementById("user");
-let fullname = document.getElementById("fname");
-let password = document.getElementById("pass");
+let usern = document.getElementById("uname");
 
 function login(){
 
 }
 
 function daftar(){
-    localStorage.setItem("username", username.value);
-    localStorage.setItem("full name", fullname.value);
-    localStorage.setItem("password", password.value);
+    
+    window.location.href = 'daftar.html';
+}
+
+function Return(){
+    window.location.href = 'index.html';
+}
+
+function daftarAcc(){
+    let username = document.getElementById("user");
+    let fullname = document.getElementById("fname");
+    let password = document.getElementById("pass");
+    localStorage.setItem("user", username.value);
+    localStorage.setItem("fname", fullname.value);
+    localStorage.setItem("pass", password.value);
+    usern.textContent = localStorage.getItem("user");
+    window.location.href = 'account.html';
 }
